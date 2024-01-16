90e666c5dbf6a066544e2a68168eca9f5aa8353f
tdietzel22 <147360049+tdietzel22@users.noreply.github.com>
2024-01-16 14:54:43 -0800

    Add example test file

---
+++ ProjectName.Solution/ProjectName.Tests/ModelTests/ClassNameTests.cs
@@ -0,0 +1,11 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ExampleName.Models;
+
+namespace ProjectName.Tests
+{
+  [TestClass]
+  public class ClassNameTests
+  {
+    // Tests will go here!
+  }
+}
