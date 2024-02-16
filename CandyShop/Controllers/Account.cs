4a7d1bf2f4fb4cb357effddab995918178d3d33e
tdietzel22 <147360049+tdietzel22@users.noreply.github.com>
2024-02-16 09:27:08 -0800

    Add login for ApplicationUser

--- CandyShop/Controllers/Account.cs
+++ CandyShop/Controllers/Account.cs
@@ -48,5 +48,31 @@ namespace Candy.Controllers
       }
     }
     public IActionResult Register() { return View(); }
+
+    [HttpPost]
+    public async Task<ActionResult> Login(LoginViewModel model)
+    {
+      if (!ModelState.IsValid)
+      {
+        return View(model);
+      }
+      else
+      {
+        Microsoft.AspNetCore.Identity.SignInResult result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, isPersistent: true, lockoutOnFailure: false);
+        if (result.Succeeded)
+        {
+          return RedirectToAction("Index", "Home");
+        }
+        else
+        {
+          ModelState.AddModelError("", "There is something wrong with your email or username. Please try again.");
+          return View(model);
+        }
+      }
+    }
+    public ActionResult Login()
+    {
+      return View();
+    }
   }
 }
