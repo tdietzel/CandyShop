7ee5d5c05008881b0fb85460fef6b1888e9be7ae
tdietzel22 <147360049+tdietzel22@users.noreply.github.com>
2024-02-19 08:10:23 -0800

    Add seed data for Treat

---
+++ CandyShop/Migrations/20240219160732_AddSeedData.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CandyShop.Migrations
+{
+    public partial class AddSeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Treats",
+                columns: new[] { "TreatId", "Description", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Made With Rich Chocolate, Silky Caramel, Chewy Nougat & Packed With Crunchy Nutty Goodness.", "Snickers" },
+                    { 2, "American candy by The Hershey Company consisting of a peanut butter cup encased in chocolate", "Reese's" },
+                    { 3, "Made of three layers of wafer separated and covered by an outer layer of chocolate", "KitKat" },
+                    { 4, "Small, fruit gum candies, similar to a jelly baby in some English-speaking countries", "Haribo" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Treats",
+                keyColumn: "TreatId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Treats",
+                keyColumn: "TreatId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Treats",
+                keyColumn: "TreatId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Treats",
+                keyColumn: "TreatId",
+                keyValue: 4);
+        }
+    }
+}
--- CandyShop/Models/CandyShopContext.cs
+++ CandyShop/Models/CandyShopContext.cs
@@ -9,5 +9,18 @@ namespace Candy.Models
     public DbSet<Flavor> Flavors { get; set; }
     public DbSet<FlavorTreat> FlavorTreats { get; set; }
     public CandyShopContext(DbContextOptions options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+      base.OnModelCreating(builder);
+
+      builder.Entity<Treat>()
+      .HasData(
+        new Treat { TreatId = 1, Name = "Snickers", Description = "Made With Rich Chocolate, Silky Caramel, Chewy Nougat & Packed With Crunchy Nutty Goodness." },
+        new Treat { TreatId = 2, Name = "Reese's", Description = "American candy by The Hershey Company consisting of a peanut butter cup encased in chocolate" },
+        new Treat { TreatId = 3, Name = "KitKat", Description = "Made of three layers of wafer separated and covered by an outer layer of chocolate" },
+        new Treat { TreatId = 4, Name = "Haribo", Description = "Small, fruit gum candies, similar to a jelly baby in some English-speaking countries"}
+      );
+    }
   }
 }
